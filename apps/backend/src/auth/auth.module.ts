7fc79468729d728941f4e9ecf2ab3e4a1ca5b1ff
LAPTOP-OOFI6RIH\carde <cardenas3213@gmail.com>
2024-03-22 23:16:07 +0100

    Converted to monorepo

--- apps/backend/src/auth/auth.module.ts
+++ apps/backend/src/auth/auth.module.ts
@@ -3,7 +3,7 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { LocalStrategy } from './local.strategy';
 import { PassportModule } from '@nestjs/passport';
-import { UsersModule } from 'src/users/users.module';
+import { UsersModule } from '../users/users.module';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
